e611942ae7fec69a951d42c0c9a20dec40f2e77d
Anil Babu <anil.mayankal@gmail.com>
2025-01-21 13:40:02 +0530

    push admin

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'api.charlinasweeps.co.in'],
+    unoptimized: true,
+  },
+  output: "export",
 };
 
 export default nextConfig;
